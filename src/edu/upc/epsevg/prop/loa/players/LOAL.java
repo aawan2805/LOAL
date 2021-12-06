49d3e0082c2c2c7e0f2238e73d66f326703eebb1
Abdullah <aabdullahsmx2014@gmail.com>
2021-12-06 15:34:36 +0100

    Implement score board method for heuristic function

--- src/edu/upc/epsevg/prop/loa/players/LOAL.java
+++ src/edu/upc/epsevg/prop/loa/players/LOAL.java
@@ -156,6 +156,49 @@ public class LOAL implements IPlayer, IAuto {
         return alfa;
     }
     
+    public int puntuarTablero(CellType jugador, GameStatus s, ArrayList<Point> pendingAmazons, DisjointSet ds,  int numeroSets){
+        int valorMinimo=0;
+        //System.out.println(s);
+        ArrayList<ArrayList<Integer>> list_disjoint=ds.get_set();
+        ArrayList<Integer> auxPrimaria, auxSecundaria;
+        int prueba = 0; 
+        for (int i = 0; i < list_disjoint.size()-1; i++) {
+            prueba++;
+            auxPrimaria = list_disjoint.get(i);
+            for (int j = i+1; j < list_disjoint.size(); j++) {
+                prueba++;
+                auxSecundaria=list_disjoint.get(j);
+                int valorMinimoListaAuxiliar2 = -1; 
+                    for (int k = 0; k < auxPrimaria.size(); k++) {
+                        prueba++;
+                        Point primario = pendingAmazons.get(auxPrimaria.get(k));
+                        int valorMinimoLista = -1;
+                           for (int l = 0; l < auxSecundaria.size(); l++) {
+                               prueba++;
+                               Point secundario = pendingAmazons.get(auxSecundaria.get(l));
+                               int valor = (int) primario.distance(secundario);
+                               if(valorMinimoLista == -1 ) valorMinimoLista = valor;
+                               else if(valor<valorMinimoLista) valorMinimoLista = valor;
+    //                           System.out.println(auxPrimaria.get(k) + " " + auxSecundaria.get(l) + " " + valor);
+                           }
+    //                    System.out.println("valorMinimo " + valorMinimoLista);
+                        if(valorMinimoListaAuxiliar2 == -1 ) valorMinimoListaAuxiliar2 = valorMinimoLista;
+                        else if(valorMinimoLista<valorMinimoListaAuxiliar2) valorMinimoListaAuxiliar2 = valorMinimoLista;
+                }  
+//                System.out.println("ValorMinimoParaLaArray: " + valorMinimoListaAuxiliar2);   
+                    
+                valorMinimo += valorMinimoListaAuxiliar2;
+            }
+        }
+        
+//        System.out.println(prueba);
+      //  System.out.println(valorMinimo);
+        int valorFinal = s.getNumberOfPiecesPerColor(jugador) - numeroSets + (100-valorMinimo);
+        //System.out.println(valorFinal);
+        //System.out.println(" ");
+        return valorFinal;
+    }
+    
     public int Eval(GameStatus s, CellType jugador){
         
         int qn = s.getNumberOfPiecesPerColor(jugador);
