d92e1fb33326975a5a614b2e92254520ac87c557
Abdullah <aabdullahsmx2014@gmail.com>
2021-12-06 15:31:21 +0100

    New approach to implement disjoint set

--- src/edu/upc/epsevg/prop/loa/players/DisjointSet.java
+++ src/edu/upc/epsevg/prop/loa/players/DisjointSet.java
@@ -8,90 +8,108 @@ package edu.upc.epsevg.prop.loa.players;
 import java.awt.Point;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *
  * @author Abdullah Bashir Yasmin, Mario Konstanty Kochan
  */
 public class DisjointSet {
-    int[] rank, parent;
-    int n;
-    
-    public DisjointSet(int n){
-        this.rank = new int[n];
-        this.parent = new int[n];
-        this.n = n;
-        makeSet();
-    }
+    private List<Map<Integer, Set<Integer>>> disjointSet;
  
-    /**
-     * Rellena el parent, lo que dice es que cada punto es parent de sí mismo.
-     * @param universe Lista de puntos iniciales
-     */
-    public void makeSet(){
-        for(int i = 0; i < this.n; i++) parent[i] = i;
-    }
- 
-    /**
-     * Busca el padre del punto.
-     * @param k El punto a buscar
-     * @return El padre del punto k.
-     */
-    public int Find(int p){
-        if(parent[p] != p) parent[p] = Find(parent[p]);
-        
-        // recur for the parent until we find the root
-        return parent[p];
-    }
- 
-    /**
-     * Añade al mismo conjunto el punto a y b.
-     * @param a Punto a unir.
-     * @param b Punto a unir.
-     */
-    public void Union(int x, int y)
+    public DisjointSet()
     {
-        // Find representatives of two sets
-        int xRoot = Find(x), yRoot = Find(y);
-  
-        // Elements are in the same set, no need
-        // to unite anything.
-        if (xRoot == yRoot)
-            return;
-  
-        // If x's rank is less than y's rank
-        if (rank[xRoot] < rank[yRoot])
-  
-            // Then move x under y  so that depth
-            // of tree remains less
-            parent[xRoot] = yRoot;
-  
-        // Else if y's rank is less than x's rank
-        else if (rank[yRoot] < rank[xRoot])
-  
-            // Then move y under x so that depth of
-            // tree remains less
-            parent[yRoot] = xRoot;
-  
-        else // if ranks are the same
+        disjointSet = new ArrayList<Map<Integer, Set<Integer>>>();
+    }
+ 
+    public void create_set(int element)
+    {
+        Map<Integer, Set<Integer>> map = new HashMap<Integer, Set<Integer>>();
+        Set<Integer> set = new HashSet<Integer>();
+        set.add(element);
+        map.put(element, set);
+        disjointSet.add(map);
+    }
+ 
+    public void union(int first, int second)
+    {
+        int first_rep = find_set(first);
+        int second_rep = find_set(second);
+ 
+        Set<Integer> first_set = null;
+        Set<Integer> second_set = null;
+ 
+        for (int index = 0; index < disjointSet.size(); index++)
         {
-            // Then move y under x (doesn't matter
-            // which one goes where)
-            parent[yRoot] = xRoot;
-  
-            // And increment the result tree's
-            // rank by 1
-            rank[xRoot] = rank[xRoot] + 1;
+            Map<Integer, Set<Integer>> map = disjointSet.get(index);
+            if (map.containsKey(first_rep))
+            {
+                first_set = map.get(first_rep);
+            } else if (map.containsKey(second_rep))
+            { 
+                second_set = map.get(second_rep);
+            }
         }
+ 
+        if (first_set != null && second_set != null)
+        first_set.addAll(second_set);
+ 
+        for (int index = 0; index < disjointSet.size(); index++)
+        {
+            Map<Integer, Set<Integer>> map = disjointSet.get(index);
+            if (map.containsKey(first_rep))
+            {
+                map.put(first_rep, first_set);
+            } else if (map.containsKey(second_rep))
+            {
+                map.remove(second_rep);
+                disjointSet.remove(index);
+            }
+        }
+ 
+        return;
+    }
+    public ArrayList<ArrayList<Integer>> get_set(){
+        ArrayList<ArrayList<Integer>> awan;
+        awan = new ArrayList();
+        for (int i = 0; i < disjointSet.size(); i++) {
+            Map<Integer, Set<Integer>> aloha = disjointSet.get(i);
+            aloha.forEach((key,value)->{
+               Set<Integer> auxiliar = value;
+               ArrayList<Integer> auxList;
+               auxList = new ArrayList();
+               auxiliar.forEach((valor)->{
+                   auxList.add(valor);
+               });
+               awan.add(auxList);
+            });
+        }
+        return awan;
     }
     
-    /**
-     * Imprime por pantalla el conjunto parent.
-     */
-    public void printSet(){
-        for (int i = 0; i < this.n; i++) {
-            System.out.println(parent[i] + " => " + rank[i]);
+    public int find_set(int element)
+    {
+        for (int index = 0; index < disjointSet.size(); index++)
+        {
+            Map<Integer, Set<Integer>> map = disjointSet.get(index);
+            Set<Integer> keySet = map.keySet();
+            for (Integer key : keySet)
+            {
+                Set<Integer> set = map.get(key);
+                if (set.contains(element))
+                {
+                    return key;
+                }
+            }
         }
+        return -1;
+    }
+ 
+    public int getNumberofDisjointSets()
+    {
+        return disjointSet.size();
     }
 }
